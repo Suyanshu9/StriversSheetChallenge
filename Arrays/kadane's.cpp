bc9469788be75436a79c9843892a1a8700b9224f
Suryanshu Dobai <113342323+Suyanshu9@users.noreply.github.com>
2023-07-11 22:20:40 +0530

    Create kadane's.cpp

---
+++ Arrays/kadane's.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN;
+        int sum = 0;
+        int n = nums.size();
+        for(int i = 0; i< n ;i++){
+            sum += nums[i];
+
+            if(sum > ans){
+                ans = sum;
+            }
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+
+        // if(ans < 0){
+        //     ans = 0;
+        // }
+
+        return ans;
+    }
+};
